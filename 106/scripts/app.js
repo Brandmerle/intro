ea8cdf1edab0f6f18b4556a4ad38765396f732b5
Brandon Merle <brandon.merle@yahoo.com>
2025-01-18 11:50:10 -0500

    Add files via upload

--- 106/scripts/app.js
+++ 106/scripts/app.js
@@ -1,10 +1,20 @@
-function sayHello(){
-    console.log("Hello");
+function saveTask(){
+    console.log("saving task");
+    //get values
+
+    //built an objecct
+
+    //save to server
+
+    //display the data received from server
 }
 
 function init(){
-    console.log("hello world!")
-    sayHello();
+    console.log("init");
+    //load data
+
+    //hook events
+    $("#btnSave").click(saveTask);
 }
 
 window.onload = init; //-- this will wait until HTML and CSS gets resolved to run the JS
